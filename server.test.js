364ff00765969e6a09367f9d9696145e5e843b20
gauridighe347 <gauridighe055@gmail.com>
2025-11-26 10:38:23 +0530

    Initial project commit

--- server.test.js
+++ server.test.js
@@ -5,6 +5,6 @@ describe("GET /", () => {
   test("should return 200 OK", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe("Hello from Node.js + Docker + Kubernetes + Jenkins CI/CD!");
+    expect(res.text).toBe("Hello from Node.js + Jenkins CI/CD!");
   });
 });
